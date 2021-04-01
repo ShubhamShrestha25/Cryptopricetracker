0f695a65d4b4631c9a33f51f04a88d8da95335eb
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-04-01 19:43:58 +0545

    Final commit

--- components/coins/Coins.js
+++ components/coins/Coins.js
@@ -1,4 +1,5 @@
 import styles from "./Coins.module.css";
+import Link from "next/link";
 
 const Coins = ({
   name,
@@ -11,32 +12,36 @@ const Coins = ({
   id,
 }) => {
   return (
-    <div className={styles.coin_container}>
-      <div className={styles.coin_row}>
-        <div className={styles.coin}>
-          <img src={image} alt={name} className={styles.coin_img} />
-          <h1 className={styles.coin_h1}>{name}</h1>
-          <p className={styles.coin_symbol}>{symbol}</p>
-        </div>
-        <div className={styles.coin_data}>
-          <p className={styles.coin_price}>${price}</p>
-          <p className={styles.coin_volume}>${volume.toLocaleString()}</p>
+    <Link href="/coin/[id]" as={`/coin/${id}`}>
+      <a>
+        <div className={styles.coin_container}>
+          <div className={styles.coin_row}>
+            <div className={styles.coin}>
+              <img src={image} alt={name} className={styles.coin_img} />
+              <h1 className={styles.coin_h1}>{name}</h1>
+              <p className={styles.coin_symbol}>{symbol}</p>
+            </div>
+            <div className={styles.coin_data}>
+              <p className={styles.coin_price}>${price}</p>
+              <p className={styles.coin_volume}>${volume.toLocaleString()}</p>
 
-          {priceChange < 0 ? (
-            <p className={(styles.coin_percent, styles.red)}>
-              {priceChange.toFixed(2)}%
-            </p>
-          ) : (
-            <p className={(styles.coin_percent, styles.green)}>
-              {priceChange.toFixed(2)}%
-            </p>
-          )}
-          <p className={styles.coin_marketcap}>
-            Mkt Cap: ${marketcap.toLocaleString()}
-          </p>
+              {priceChange < 0 ? (
+                <p className={(styles.coin_percent, styles.red)}>
+                  {priceChange.toFixed(2)}%
+                </p>
+              ) : (
+                <p className={(styles.coin_percent, styles.green)}>
+                  {priceChange.toFixed(2)}%
+                </p>
+              )}
+              <p className={styles.coin_marketcap}>
+                Mkt Cap: ${marketcap.toLocaleString()}
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </a>
+    </Link>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,26 @@
-import Head from "next/head";
+import { useState } from "react";
 import CoinList from "../components/coins/CoinList";
 import SearchBar from "../components/SearchBar/SearchBar";
+import Layout from "../components/SearchBar/Layout";
 
 export default function Home({ data }) {
-  console.log(data);
+  const [search, setSearch] = useState("");
+
+  const allCoins = data.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    setSearch(e.target.value.toLowerCase());
+  };
   return (
     <div>
-      <Head>
-        <title>Crypto Tracker</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <SearchBar type="text" placeholder="Search" />
-      <CoinList filteredCoins={data} />
+      <Layout />
+      <div className="coin_app">
+        <SearchBar type="text" placeholder="Search" onChange={handleChange} />
+        <CoinList filteredCoins={allCoins} />
+      </div>
     </div>
   );
 }
